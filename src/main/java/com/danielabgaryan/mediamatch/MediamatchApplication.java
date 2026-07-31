ac79a6f9c2dd48e1fa204e8c3c87621a77db8093
Daniel Abgaryan <Dabgaryan04@gmail.com>
2026-07-31 12:59:30 -0700

    Initialize MediaMatch project skeleton

---
+++ src/main/java/com/danielabgaryan/mediamatch/MediamatchApplication.java
@@ -0,0 +1,13 @@
+package com.danielabgaryan.mediamatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediamatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediamatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielabgaryan/mediamatch/MediamatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielabgaryan.mediamatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediamatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
